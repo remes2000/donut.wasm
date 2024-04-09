8cf44c7c6249d4fa3ca33dba06258f14203da2a7
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2024-04-10 00:43:23 +0200

    feat: add support for touch api

--- src/mouse-rotation-controller.mjs
+++ src/mouse-rotation-controller.mjs
@@ -1,5 +1,10 @@
 import { ENTER_MOUSE_ROTATION_MODE, EXIT_MODE, EventBus } from "./event-bus.mjs";
 
+const getCoordinates = (event) => {
+  const { clientX, clientY } = event instanceof TouchEvent ? event.touches[0] : event;
+  return [clientX, clientY];
+}
+
 export class MouseRotationController {
   donut;
   lockPoint;
@@ -26,27 +31,36 @@ export class MouseRotationController {
     this.display.addEventListener('mousedown', this.lock);
     this.display.addEventListener('mouseup', this.release);
     this.display.addEventListener('mousemove', this.rotate);
+
+    this.display.addEventListener('touchstart', this.lock);
+    this.display.addEventListener('touchend', this.release);
+    this.display.addEventListener('touchmove', this.rotate);
   }
 
   pause() {
     this.display.removeEventListener('mousedown', this.lock);
     this.display.removeEventListener('mouseup', this.release);
     this.display.removeEventListener('mousemove', this.rotate);
+
+    this.display.removeEventListener('touchstart', this.lock);
+    this.display.removeEventListener('touchend', this.release);
+    this.display.removeEventListener('touchmove', this.rotate);
   }
 
-  lock = ({ clientX, clientY }) => {
-    this.lockPoint = [clientX, clientY];
+  lock = (event) => {
+    this.lockPoint = getCoordinates(event);
   }
 
   release = () => {
     this.lockPoint = null;
   }
 
-  rotate = ({ clientX, clientY }) => {
+  rotate = (event) => {
     if (!this.lockPoint) {
       return;
     }
     const [lockX, lockY] = this.lockPoint;
+    const [clientX, clientY] = getCoordinates(event);
     const [dx, dy] = [clientX - lockX, clientY - lockY];
     this.donut.rotate(dy * this.rotationStep, dx * this.rotationStep);
     this.lockPoint = [clientX, clientY];
